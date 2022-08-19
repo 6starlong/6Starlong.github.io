2bc1505ab30ff614e3c6228d4024a3aeee056871
Starlong <starlong.lu@gmail.com>
2022-08-19 12:16:34 +0800

    fix: vue-router scrollBehavior

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,19 @@ import 'uno.css'
 
 const routes = setupLayouts(generatedRoutes)
 
+const scrollBehavior = async (to: any, from: any, savedPosition: any) => {
+  if (savedPosition) { return { ...savedPosition, behavior: 'smooth' } }
+  else {
+    // https://github.com/vuejs/router/issues/1147#issuecomment-939265893
+    await new Promise(resolve => setTimeout(resolve, 300))
+    return { top: 0, behavior: 'smooth' }
+  }
+}
+
 // https://github.com/antfu/vite-ssg
 export const createApp = ViteSSG(
   App,
-  { routes, base: import.meta.env.BASE_URL },
+  { routes, scrollBehavior, base: import.meta.env.BASE_URL },
   (ctx) => {
     // install all modules under `modules/`
     Object.values(import.meta.glob<{ install: UserModule }>('./modules/*.ts', { eager: true }))
